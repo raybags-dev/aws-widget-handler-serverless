c9ad9e3fdac0849a4f49079c543bba1b24bc6d51
EC2 Default User <ec2-user@ip-172-31-45-20.us-east-2.compute.internal>
2022-10-02 18:57:05 +0000

    Initial commit

---
+++ test/my_widget_service.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const MyWidgetService = require('../lib/my_widget_service-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my_widget_service-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new MyWidgetService.MyWidgetServiceStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
